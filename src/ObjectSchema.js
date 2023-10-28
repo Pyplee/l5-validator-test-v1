b057448b528ad6a2772e089470ef60520e22a324
pyplee <dekimiq@gmail.com>
2023-10-28 20:54:05 +0300

    add comment about every

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -17,6 +17,19 @@ class ObjectSchrema {
     }
 
     return schemaKeys.every((key) => this.validations[key].isValid(data[key]));
+
+    //  const schema3 = validator.object().shape({
+    //   id: validator.number().even(),
+    //   basket: validator.array().length(2),
+    //   payment: validator.array().length(2),
+    // });
+
+    // schema1.isValid({ id: 11, basket: ['potatos', 'tomatos', 'oranges'] }), true);
+    // schema1.isValid({ id: 12, basket: ['potatos', 'tomatos', 'oranges'] }), false);
+
+    // [id, basket].every((id) => v.number().odd()[id].isValid(11))
+    // [id, basket].every((basket) =>
+    // v.array().length(3)[basket].isValid(['potatos', 'tomatos', 'oranges']))
   }
 }
 
